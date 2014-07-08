789a508a032eea9d1dbb2aa5e4a7639bf1e4b82f
Lakshmi Raghavan <rajulonline@gmail.com>
2014-07-07 18:03:30 -0700

    simple cross browser testing with browser stack

---
+++ cross_browser_testing/app/watir_browser_stack_script.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+# Input capabilities
+caps = Selenium::WebDriver::Remote::Capabilities.new
+caps["browser"] = "IE"
+caps["browser_version"] = "7.0"
+caps["os"] = "Windows"
+caps["os_version"] = "XP"
+caps["browserstack.debug"] = "true"
+caps["name"] = "Testing Selenium 2 with Ruby on BrowserStack"
+
+driver = Selenium::WebDriver.for(:remote,
+  :url => "http://lax17:fmtGZEtjXJqiSUSTizSP@hub.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
+driver.navigate.to "http://www.google.com"
+element.send_keys "Testing"
+element.submit
+puts driver.title
+driver.quit
