350bbb43f2011873476f4d3d592f49762eabc042
Lakshmi Raghavan <rajulonline@gmail.com>
2014-07-07 18:05:26 -0700

    simple cross browser testing script on browser stack

---
+++ cross_browser_testing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrossBrowserTesting::Application.config.secret_token = '78839beb0c9857230ada59233affa9252acc3e93984313ca9f068f90ffe2b6ce56c5758b11fa9be45106f6c282fbc4d9ddbdf95bf58586ec41bb45c81426d30d'
